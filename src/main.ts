2e67819490bcc005eb9d2fd4277f8c8517c78270
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-08 04:02:35 +0100

    swagger Docs added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('ORG table')
+    .setDescription(' Organization API ')
+    .setVersion('1.0')
+    .addTag('Organization and Employee CRUD')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3003);
 }
