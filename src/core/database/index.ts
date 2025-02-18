f5c8104c912f0a47d66aebd53718031e28bcdce0
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-18 14:34:40 +0100

    added log and activityModel to database index

--- src/core/database/index.ts
+++ src/core/database/index.ts
@@ -1,1 +1,3 @@
 export { default as Organization } from './models/organization.model';
+export { default as Log } from './models/log.model';
+export { default as ActivityModel } from './models/activity-log.model';
