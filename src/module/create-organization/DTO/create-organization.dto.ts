a1ba0c5f6512c183f3777edd3b4d8455798e0e8a
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-02-26 09:52:46 +0100

    fixing organization module

--- src/module/create-organization/DTO/create-organization.dto.ts
+++ src/module/create-organization/DTO/create-organization.dto.ts
@@ -1,23 +1,18 @@
-import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { IsString, IsEmail } from 'class-validator';
 
 export class CreateOrganizationDto {
-  @IsNotEmpty({ message: 'First name is required.' })
   @IsString()
   firstName: string;
 
-  @IsNotEmpty({ message: 'Last name is required.' })
   @IsString()
   lastName: string;
 
-  @IsNotEmpty({ message: 'Address is required.' })
   @IsString()
   address: string;
 
-  @IsNotEmpty({ message: 'Industry is required.' })
   @IsString()
   industry: string;
 
-  @IsNotEmpty({ message: 'Email is required.' })
   @IsEmail({}, { message: 'Please provide a valid email address.' })
   email: string;
 }
