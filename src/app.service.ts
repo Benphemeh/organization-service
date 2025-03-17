62d6ce9233d30d9c5eac306669dccdede32a0aa7
Adewole oluwanifemi <beniphemeh11@yahoo.com>
2025-03-17 10:37:33 +0100

    prettier and Eslint

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'YAY!!! Welcome to Organization service!';
+    return 'YAY!! Welcome to Organization service!';
   }
 }
